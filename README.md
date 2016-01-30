9a05902dbce989cf58cbcb1c45c24763dd2e886b
mattiashaal <mattias.haal@grandpublic.se>
2016-01-30 23:34:24 +0100

    Project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nano.js
+
+Nano is a JavaScript library for DOM manipulation with browser support for ie8+, moz5+, o10+, webkit.
---
+++ src/nano.js
@@ -0,0 +1,329 @@
+(function (root) {
+
+    'use strict';
+
+    function nano(selector) {
+        return new dom(selector);
+    }
+
+    /**
+     * Selector method
+     * @returns: A collection of nodes
+     */
+    var dom = function (selector) {
+        if (!selector) {
+            return this;
+        }
+        this.length = 0;
+        this.nodes = [];
+        if (selector instanceof HTMLElement || selector instanceof NodeList) {
+            this.nodes = (selector.length > 1) ? [].slice.call(selector) : [selector];
+        } else if (typeof selector === 'string') {
+            if (selector[0] === '<' && selector[selector.length - 1] === '>') {
+                this.nodes = [nano.createNode(selector)];
+            } else {
+                this.nodes = [].slice.call(document.querySelectorAll(selector));
+            }
+        }
+        if (this.nodes.length) {
+            this.length = this.nodes.length;
+            for (var i = 0; i < this.nodes.length; i++) {
+                this[i] = this.nodes[i];
+            }
+        }
+        return this;
+    };
+
+    nano.createNode = function (html) {
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div.firstChild;
+    }
+
+    nano.each = function (collection, callback) {
+        var l = collection.length;
+        for (var i = 0; i < l; i++) {
+            callback.call(collection[i], collection, i);
+        }
+    }
+
+    /**
+     * DOM manipulation methods
+     */
+    nano.fn = dom.prototype = {
+
+        /**
+         * Iterates through a collection and calls the callback method on each
+         */
+        each: function (callback) {
+            nano.each(this, callback);
+        },
+
+        /**
+         * Returns first element in array
+         */
+        first: function () {
+            return nano(this[0]);
+        },
+
+        /**
+         * Returns last element in array
+         */
+        last: function () {
+            return nano(this[this.length - 1]);
+        },
+
+        /**
+         * Show elements in DOM
+         */
+        show: function () {
+            var l = this.length;
+            while (l--) {
+                this[l].style.display = 'block';
+            }
+            return this;
+        },
+
+        /**
+         * Hide elements from DOM
+         */
+        hide: function () {
+            var l = this.length;
+            while (l--) {
+                this[l].style.display = 'none';
+            }
+            return this;
+        },
+
+        /**
+         * Check if element has class
+         */
+        hasClass: function (className) {
+            if (document.documentElement.classList) {
+                return this[0].classList.contains(className);
+            } else {
+                return new RegExp('(^|\\s)' + className + '(\\s|$)').test(this[0].className);
+            }
+        },
+
+        /**
+         * Add class to elements
+         */
+        addClass: function (className) {
+            if (!this.hasClass(className)) {
+                if (document.documentElement.classList) {
+                    return this.each(function() {
+                        this.classList.add(className);
+                    });
+                } else {
+                    return this.each(function() {
+                        this.className += ' ' + className;
+                    });
+                }
+            }
+        },
+
+        /**
+         * Remove class from elements
+         */
+        removeClass: function (className) {
+            if (this.hasClass(className)) {
+                if (document.documentElement.classList) {
+                    return this.each(function() {
+                        this.classList.remove(className);
+                    }
+                } else {
+                    return this.each(function() {
+                        this.className = this.className.replace(new RegExp('(^|\\s)*' + className + '(\\s|$)*', 'g'), '');
+                    }
+                }
+            }
+        },
+
+        /**
+         * Toggle class on elements
+         */
+        toggleClass: function (className) {
+            if (document.documentElement.classList) {
+                return this.each(function() {
+                    this.classList.toggle(className);
+                }
+            } else {
+                return this.each(function() {
+                    if (this.hasClass(className)) {
+                        this.removeClass(className);
+                    } else {
+                        this.addClass(className);
+                    }
+                }
+            }
+        },
+
+        /**
+         * Callback method on DOMContentLoaded
+         * TODO: Add this[0] instead of document to function
+         */
+        ready: function (callback) {
+            document.addEventListener("DOMContentLoaded", callback);
+        },
+
+        /**
+         * Add event
+         */
+        on: function (type, callback) {
+            if (document.addEventListener) {
+                return this.each(function () {
+                    this.addEventListener(type, callback, false);
+                });
+            } else if (document.attachEvent) {
+                return this.each(function () {
+                    this.attachEvent('on' + type, callback);
+                });
+            }
+        },
+
+        /**
+         * Remove event
+         */
+        off: function () {
+            if (document.removeEventListener) {
+                return this.each(function () {
+                    this.removeEventListener(type, callback, false);
+                });
+            } else if (document.detachEvent) {
+                return this.each(function () {
+                    this.detachEvent('on' + type, callback);
+                });
+            }
+        },
+
+        /**
+         *
+         */
+        text: function () {},
+
+        /**
+         *
+         */
+        html: function () {},
+
+        /**
+         *
+         */
+        attr: function () {},
+
+        /**
+         *
+         */
+        removeAttr: function () {},
+
+        /**
+         *
+         */
+        append: function () {},
+
+        /**
+         *
+         */
+        appendTo: function () {},
+
+        /**
+         *
+         */
+        matches: function () {},
+
+        /**
+         *
+         */
+        css: function () {},
+
+        /**
+         *
+         */
+        data: function () {},
+
+        /**
+         *
+         */
+        removeData: function () {},
+
+        /**
+         *
+         */
+        empty: function () {},
+
+        /**
+         *
+         */
+        width: function () {},
+
+        /**
+         *
+         */
+        height: function () {},
+
+        /**
+         *
+         */
+        innerWidth: function () {},
+
+        /**
+         *
+         */
+        innerHeight: function () {},
+
+        /**
+         *
+         */
+        viewWidth: function () {},
+
+        /**
+         *
+         */
+        viewHeight: function () {},
+
+        /**
+         *
+         */
+        inView: function () {},
+
+        /**
+         *
+         */
+        insertBefore: function () {},
+
+        /**
+         *
+         */
+        insertAfter: function () {},
+
+        /**
+         *
+         */
+        prepend: function () {},
+
+        /**
+         *
+         */
+        prependTo: function () {},
+
+        /**
+         *
+         */
+        add: function () {},
+
+        /**
+         *
+         */
+        remove: function () {},
+
+        /**
+         *
+         */
+        next: function () {}
+
+    }
+
+    root.nano = root.$ = nano;
+
+})(this);
