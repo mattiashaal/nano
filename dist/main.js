c0c24dbbef9964612dbfef7a2d02ce90d5b7c010
mattiashaal <mattias.haal@grandpublic.se>
2016-02-15 21:28:09 +0100

    Clean up

--- dist/main.js
+++ dist/main.js
@@ -1,38 +1,11 @@
-// var n = $('.nano');
-// console.log(n);
-//
-// if (n.first().hasClass('nano--masthead')) {
-//     console.log('yes!');
-// }
-//
-// n.addClass('demo');
-//
-// n.first().on('click', function() {
-//     n.first().hide();
-// });
-//
-// console.log(n.last());
-//
-// n.last().on('click', function() {
-//     n.first().show();
-// });
-//
-// $.ready(function() {
-//     console.log('ready!');
-// });
-//
-// var elem = $('.inview');
-// console.log(elem[0]);
-//
-// var demo = $('.inview').inview();
-// console.log(demo);
+$.ready(function() {
+    console.log('ready!');
+});
 
-// var elem = nano.get('.class')
-// nano.inview(nano.get('.class'));
-//
-// $('.class').inview();
+var demo = $('.inview').inview();
+console.log(demo);
 
-var elems = $('.nano--masthead');
+var elems = $('.nano');
 console.log(elems);
 
 var id = $('#app');
